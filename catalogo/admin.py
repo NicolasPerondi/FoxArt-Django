9c22fce29da75c47b2f9821d16e8e2374dcc7de3
Nicolas Perondi <Perondi.Nicolas@gmail.com>
2022-12-09 08:49:46 -0300

    first commit

--- catalogo/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Product
-# Register your models here.
-
-# Register your models here.
-class ProductAdmin(admin.ModelAdmin):
-    readonly_fields = ('create','update')
-
-admin.site.register(Product,ProductAdmin)
--- catalogo/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from .models import Product
-
-# Create your views here.
-def catalogo(request):
-    products = Product.objects.all()
-    return render(request, "catalogo/catalogo.html", {'products': products})
